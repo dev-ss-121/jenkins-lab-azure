39e132bc169c9d1ad0103b8b1118ce6173c05188
dev-ss-121 <shahid1029121@hotmail.com>
2025-08-15 15:01:54 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,13 +9,13 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "<your subscription id here>"
+  subscription_id = "f4bcc67e-b65f-4cb6-9b0a-85dd34284dc9"
 }
 
 terraform {
   backend "azurerm" {
     resource_group_name  = "RG1"
-    storage_account_name = "<your storage account>"
+    storage_account_name = "jenkinsstate408"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
   }
@@ -25,3 +25,4 @@ resource "azurerm_resource_group" "example" {
   name     = "RG2"
   location = "westeurope"
 }
+
